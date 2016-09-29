2cd5e3cf6825c1747ee995791eeacf54a9a1eb8e
sergdort <sergdort@gmail.com>
2016-09-29 21:52:04 +0100

    Use fork

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentMySQL",
     dependencies: [
-   		.Package(url: "https://github.com/vapor/mysql.git", majorVersion: 1),
+   		.Package(url: "https://github.com/sergdort/mysql.git", majorVersion: 1),
    		.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 1),
     ]
 )
